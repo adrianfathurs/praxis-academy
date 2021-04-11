4c8213fd878711b5e0b47f5d4c315ec7e3583c9b
Inabiel <nabiel.p@students.amikom.ac.id>
2021-04-11 23:42:10 +0700

    adding events and user controller.

--- projectPraxis/sponsorin/backend/controllers/v1/auth.js
+++ projectPraxis/sponsorin/backend/controllers/v1/auth.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 const verifySignUp = async (req, res) => {
   try {
-    let { nama, email, noTelpon, password } = req.body;
+    let { nama, email, notelpon, password } = req.body;
     const result = await prisma.user.findUnique({
       where: {
         email: email,
@@ -25,13 +25,19 @@ const verifySignUp = async (req, res) => {
       data: {
         nama: nama,
         email: email,
-        noTelpon: noTelpon,
+        noTelpon: notelpon,
         password: hashedPassword,
       },
     });
-    const token = jwt.sign({ userID: user.id }, process.env.TOKEN_SECRET);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Ada Kesalahan di backend" });
+    }
 
-    return res.status(200).json({ success: true, token: token });
+    return res
+      .status(200)
+      .json({ success: true, msg: "Succesfully registered to Sponsorin" });
   } catch (e) {
     console.log(e);
   }
@@ -44,19 +50,32 @@ const verifyLogIn = async (req, res) => {
       where: {
         email: email,
       },
+      include: {
+        roles: true,
+      },
     });
-    console.log(user);
     if (!user) {
       return res.status(400).json({ success: false, msg: "User not found" });
     }
-
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res
         .status(400)
         .json({ success: false, msg: "Password is wrong!" });
     }
-    const token = jwt.sign({ userID: user.id }, process.env.TOKEN_SECRET);
+    const token = await jwt.sign(
+      {
+        id: user.id,
+        nama: user.nama,
+        email: user.email,
+        roles: user.rolesId,
+        desc: user.deskripsi,
+      },
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: 86400, //24h expired
+      }
+    );
     return res.status(200).json({ success: true, jwt: token });
   } catch (e) {
     console.log(e);
